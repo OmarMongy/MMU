9764f29b36987638f092c2c880544477a2e36f1f
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2026-02-10 01:09:15 +0200

    Create mmu_top.sv

---
+++ src/mmu_top.sv
@@ -0,0 +1,43 @@
+module mmu_top (
+    input  logic clk,
+    input  logic rst_n,
+
+    // Control
+    input  logic       valid_in,
+    input  logic [1:0] op_code,
+    output logic       valid_out,
+
+    // MMU data
+    input  logic [7:0]  mmu_in   [0:11][0:6][0:3],
+    input  logic [7:0]  mmu_w    [0:11][0:3],
+    input  logic [15:0] mmu_bias [0:11][0:6],
+    output logic [18:0] mmu_out  [0:6]
+);
+    wire start_op;
+
+    // ----------------------------
+    // Flag controller
+    // ----------------------------
+    mmu_flag_ctrl u_flag_ctrl (
+        .clk       (clk),
+        .rst_n     (rst_n),
+        .valid_in  (valid_in),
+        .op_code   (op_code),
+        .start_op  (start_op),
+        .valid_out (valid_out)
+    );
+
+    // ----------------------------
+    // MMU Datapath
+    // ----------------------------
+    mmu mmu_datapath (
+        .clk       (clk),
+        .rst_n     (rst_n),
+        .en        (start_op),
+        .mmu_in    (mmu_in),
+        .mmu_w     (mmu_w),
+        .mmu_bias  (mmu_bias),
+        .mmu_out   (mmu_out)
+    );
+
+endmodule
