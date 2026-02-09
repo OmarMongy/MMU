5fff6ef85b1d9fd9354915b5d13e65a386a8e658
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2026-02-09 13:41:41 +0200

    Update d_ff.sv

--- d_ff.sv
+++ d_ff.sv
@@ -1,15 +1,14 @@
 module ff (clk, rst_n, d, q);
     input clk;
     input rst_n;
+    input ena,
     input [15:0] d;
     output reg [15:0] q;
 
     always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            q <= 0;
-        end else begin
+        if (!rst_n) 
+            q <= 0; 
+        else if (ena) 
             q <= d;
         end
-    end
-    
 endmodule
