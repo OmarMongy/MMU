c1f3e5fb4e2db64aa42648acb2a4f0f6cc920eca
Omar Ashraf Abd El Mongy <128651431+OmarMongy@users.noreply.github.com>
2026-02-10 01:10:55 +0200

    Update mmu.sv

--- src/mmu.sv
+++ src/mmu.sv
@@ -1,9 +1,10 @@
-module mmu_top (clk, rst_n, mmu_in, mmu_w, mmu_bias, mmu_out);
+module mmu_top (clk, rst_n, en, mmu_in, mmu_w, mmu_bias, mmu_out);
 
     input clk, rst_n;
     input  [7:0]  mmu_in     [0:11][0:6][0:3];
     input  [7:0]  mmu_w      [0:11][0:3];
     input  [15:0] mmu_bias   [0:11][0:6];
+    input         en;
     output [18:0] mmu_out    [0:6];
 
     wire [15:0] pe_out       [0:11][0:6];
@@ -19,8 +20,8 @@ module mmu_top (clk, rst_n, mmu_in, mmu_w, mmu_bias, mmu_out);
         end
         for (i=0; i<12; i++) begin: Buff
             for (j=0; j<7; j++) begin
-                ff buff (.clk(clk), .rst_n(rst_n), .d(pe_out[i][j]), .q(buff_out[i][j]));
-                ff fb_buff (.clk(clk), .rst_n(rst_n), .d(acc_res[i][j]), .q(fb_buff_out[i][j]));
+                ff buff (.clk(clk), .rst_n(rst_n), .ena(en), .d(pe_out[i][j]), .q(buff_out[i][j]));
+                ff fb_buff (.clk(clk), .rst_n(rst_n), .ena(en), .d(acc_res[i][j]), .q(fb_buff_out[i][j]));
                 assign acc_res[i][j] = buff_out[i][j] + fb_buff_out[i][j];
             end
         end
